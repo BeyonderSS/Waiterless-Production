8db96c4c5db49f55117197f564beeda78e2f5d27
BeyonderSS <ssbeyonder@gmail.com>
2023-05-04 02:09:26 +0530

    nav half baked +home half baked + checkout half baked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,151 +1,135 @@
 import Link from "next/link";
 import { useAuth } from "../context/AuthContext";
-import React, { useState } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { BiDish } from "react-icons/bi";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Header = () => {
+  const { user, signInWithGoogle, handleSignOut } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
-const {user,signInWithGoogle,handleSignOut} = useAuth();
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <>
-  {/* component */}
-  {/* follow me on twitter @asad_codes */}
-  <div className="flex fixed flex-wrap w-full z-50">
-    <section className=" w-full">
-      {/* navbar */}
-      <nav className="flex justify-between bg-[#E8772E] text-white w-full">
-        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
-          <Link className="text-3xl cursor-pointer font-bold font-heading" href="/">
-            {/* <img class="h-9" src="logo.png" alt="logo"> */}
-            WaiterLess
-          </Link>
-          {/* Nav Links */}
-          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
-            <li>
-              <Link className="hover:text-gray-200" href="/">
-                Home
+      <motion.div
+        initial={{ opacity: 0, scale: 1, y: -200 }}
+        animate={{ opacity: 1, scale: 1, y: 0 }}
+        transition={{
+          duration: 0.5,
+          delay: 0,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className="flex fixed flex-wrap  w-full z-50"
+      >
+        <section className=" w-full ">
+          {/* navbar */}
+          <nav className="flex rounded-b-2xl justify-between bg-[#E8772E] text-white w-full">
+            <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+              <Link
+                className="text-3xl cursor-pointer font-bold font-heading"
+                href="/"
+              >
+                WaiterLess
               </Link>
-            </li>
-            <li>
-              <Link className="hover:text-gray-200" href="/Menu">
-                Menu
-              </Link>
-            </li>
-            <li>
-              <a className="hover:text-gray-200" href="#">
-                Collections
-              </a>
-            </li>
-            <li>
-              <a className="hover:text-gray-200" href="#">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-          {/* Header Icons */}
-          <div className="hidden xl:flex space-x-5 items-center">
-            <a className="hover:text-gray-200" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-            </a>
-            <a className="flex items-center hover:text-gray-200" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="flex absolute -mt-5 ml-4">
-                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
-                <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-              </span>
-            </a>
-            {/* Sign In / Register      */}
-            <a className="flex items-center hover:text-gray-200" href="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 hover:text-gray-200"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
-        {/* Responsive navbar */}
-        <a className="xl:hidden flex mr-6 items-center" href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 hover:text-gray-200"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
-          <span className="flex absolute -mt-5 ml-4">
-            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75" />
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-          </span>
-        </a>
-        <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 hover:text-gray-200"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </a>
-      </nav>
-    </section>
-  </div>
-  {/* Does this resource worth a follow? */}
- 
-</>
+              {/* Nav Links */}
+              <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+                <li>
+                  <Link className="hover:text-gray-200" href="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="hover:text-gray-200" href="/Menu">
+                    Menu
+                  </Link>
+                </li>
+                <li>
+                  <a className="hover:text-gray-200" href="#">
+                    Collections
+                  </a>
+                </li>
+              </ul>
+              {/* Header Icons */}
+              <div className="hidden xl:flex space-x-5 items-center">
+                {user && (
+                  <div className="hover:text-gray-200">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                  </div>
+                )}
+                <a className="flex items-center hover:text-gray-200" href="#">
+                  <BiDish className="h-8 w-8 hover:text-gray-200" />
+                </a>
+                {/* Sign In / Register      */}
+                <div className="flex items-center hover:text-gray-200">
+                  {user ? (
+                    <div onClick={handleSignOut} className="cursor-pointer">
+                      <img
+                        className="w-10 h-10  rounded-full"
+                        src={user?.photoURL}
+                        alt=""
+                      />{" "}
+                    </div>
+                  ) : (
+                    <div onClick={signInWithGoogle} className="cursor-pointer">
+                      <div className="relative  w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ">
+                        <svg
+                          className="absolute w-10 h-10 text-gray-400 "
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
 
+            {/* Responsive navbar */}
+            <a className="xl:hidden flex mr-6 items-center" href="#">
+              <BiDish className="h-8 w-8 hover:text-gray-200" />
+            </a>
+            <button
+              className="navbar-burger self-center mr-12 xl:hidden"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <RxHamburgerMenu className="h-7 w-7 hover:text-gray-200" />
+            </button>
+          </nav>
+          {/* responsive nav bar  */}
+          <div
+            className={`${
+              isOpen ? " translate-y-0" : "-translate-y-24"
+            } w-full absolute rounded-b-3xl transform duration-500 ease-in-out top-0 h-auto bg-[#E8772E] -z-50 block flex-grow lg:flex lg:items-center lg:w-auto`}
+          >
+            <div className="pt-20 flex flex-row space-x-20 p-5 justify-center items-center text-white text-xl uppercase font-semibold">
+            {/* <div className="cursor-pointer">Menu</div>
+            <div className="cursor-pointer">Home</div>
+            <div className="cursor-pointer">Login</div> */}
+            <p>Hey, {user.displayName} Have a Great Meal! </p>
+            </div>
+          </div>
+        </section>
+      </motion.div>
+    </>
   );
 };
 
--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,15 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FaStar, FaShoppingCart, FaPlus, FaMinus } from "react-icons/fa";
 import { MdOutlineFastfood } from "react-icons/md";
 import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { firestore } from "../utils/initFirebase";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { BarLoader } from "react-spinners";
 
 const MenuPage = () => {
   const [cartItems, setCartItems] = useState([]);
   const [menuItems, setMenuItems] = useState([]);
+  const [showCheckout, setShowCheckout] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [loading, setloading] = useState(false);
+  const menuRef = useRef();
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   useEffect(() => {
     const fetchMenuItems = async () => {
+      setloading(true);
       const menuCollectionRef = collection(firestore, "Menu");
       const menuQuery = query(menuCollectionRef);
       const menuSnapshot = await getDocs(menuQuery);
@@ -18,11 +41,11 @@ const MenuPage = () => {
         ...doc.data(),
       }));
       setMenuItems(menuData);
+      setloading(false);
     };
     fetchMenuItems();
   }, []);
 
-  
   const handleAddToCart = (item) => {
     const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
     if (existingItem) {
@@ -55,62 +78,92 @@ const MenuPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center  min-h-screen   pt-24 bg-gray-100  pb-8">
+    <div className="flex flex-col items-center  min-h-screen pt-24 bg-gray-100  pb-8">
       <h1 className="text-4xl font-bold mb-8">Menu</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {menuItems.map((item) => {
-          const existingItem = cartItems.find(
-            (cartItem) => cartItem.id === item.id
-          );
-          const quantity = existingItem ? existingItem.quantity : 0;
-          return (
-            <div
-              key={item.id}
-              className="flex flex-col pb-4 md:w-96 w-80 h-96 items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden transition duration-300 transform hover:-translate-y-1"
-            >
-              <img
-                src={item.image}
-                alt={item.dishName}
-                className="h-48 w-full object-cover mb-4"
-              />
-              <h2 className="text-lg font-bold">{item.dishName}</h2>
-              <p className="text-gray-600">₹{item.price}</p>
-              <div className="flex items-center">
-                {[...Array(Math.round(item.rating))].map((_, i) => (
-                  <FaStar key={i} className="text-yellow-500 mr-1" />
-                ))}
-              </div>
-              {quantity > 0 ? (
-                <div className="flex items-center mt-4">
-                  <button
-                    className="bg-[#E8772E] text-white py-2 px-4 rounded-full hover:bg-[#E8772E]-dark mr-2"
-                    onClick={() => handleRemoveFromCart(item)}
-                  >
-                    <FaMinus />
-                  </button>
-                  <span className="text-lg font-bold">{quantity}</span>
-                  <button
-                    className="bg-[#E8772E] text-white py-2 px-4 rounded-full hover:bg-[#E8772E]-dark ml-2"
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <BarLoader color="#E8772E" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {menuItems.map((item) => {
+            const existingItem = cartItems.find(
+              (cartItem) => cartItem.id === item.id
+            );
+            const quantity = existingItem ? existingItem.quantity : 0;
+            return (
+              <div
+                key={item.id}
+                className="flex flex-col pb-4 md:w-96 w-80 h-96 items-center justify-center bg-white rounded-lg shadow-lg overflow-hidden transition duration-300 transform "
+              >
+                <img
+                  src={item.image}
+                  alt={item.dishName}
+                  className=" w-full h-60 object-cover mb-4"
+                />
+                <h2 className="text-lg font-bold">{item.dishName}</h2>
+                <p className="text-gray-600">₹{item.price}</p>
+                <div className="flex items-center">
+                  {[...Array(Math.round(item.rating))].map((_, i) => (
+                    <FaStar key={i} className="text-yellow-500 mr-1" />
+                  ))}
+                </div>
+                {quantity > 0 ? (
+                  <div className="flex items-center mt-4">
+                    <motion.button
+                      whileTap={{ scale: 0.9 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 400,
+                        damping: 17,
+                      }}
+                      className="bg-[#E8772E] text-white py-2 px-4 rounded-full hover:bg-[#E8772E]-dark mr-2"
+                      onClick={() => handleRemoveFromCart(item)}
+                    >
+                      <FaMinus />
+                    </motion.button>
+                    <span className="text-lg font-bold">{quantity}</span>
+                    <motion.button
+                      whileTap={{ scale: 0.9 }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 400,
+                        damping: 17,
+                      }}
+                      className="bg-[#E8772E] text-white py-2 px-4 rounded-full hover:bg-[#E8772E]-dark ml-2"
+                      onClick={() => handleAddToCart(item)}
+                    >
+                      <FaPlus />
+                    </motion.button>
+                  </div>
+                ) : (
+                  <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    className="bg-[#E8772E] text-white py-2 px-4 flex flex-row justify-center items-center rounded-full mt-4 hover:bg-[#E8772E]-dark"
                     onClick={() => handleAddToCart(item)}
                   >
-                    <FaPlus />
-                  </button>
-                </div>
-              ) : (
-                <button
-                  className="bg-[#E8772E] text-white py-2 px-4 flex flex-row justify-center items-center rounded-full mt-4 hover:bg-[#E8772E]-dark"
-                  onClick={() => handleAddToCart(item)}
-                >
-                  <FaShoppingCart className="mr-2" />
-                  <span> Add to Cart</span>
-                </button>
-              )}
-            </div>
-          );
-        })}
-      </div>
+                    <FaShoppingCart className="mr-2" />
+                    <span> Add to Cart</span>
+                  </motion.button>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
       {cartItems.length > 0 && (
-        <div className="flex items-center justify-between w-full rounded-full md:px-16 px-12 py-4 bg-white shadow-lg fixed bottom-0 mb-2 ">
+        <motion.div
+          initial={{ opacity: 0, scale: 1, y: 200 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          exit={{ opacity: 0, scale: 1, y: 200 }}
+          transition={{
+            duration: 0.5,
+            delay: 0,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="flex items-center justify-between w-full rounded-t-3xl md:px-16 px-12 py-4 bg-white shadow-lg fixed bottom-0  "
+        >
           <div>
             <h2 className="text-lg font-bold">Cart</h2>
             <p className="text-gray-600">{cartItems.length} items</p>
@@ -123,13 +176,29 @@ const MenuPage = () => {
                 .toFixed(2)}
             </p>
             {cartItems.length > 0 && (
-              <button className="bg-[#E8772E] flex flex-row justify-center items-center space-x-2 text-white py-2 px-4 rounded-full mr-4 hover:bg-[#E8772E]-dark">
+              <motion.button
+                onClick={toggleMenu}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                className="bg-[#E8772E] flex flex-row justify-center items-center space-x-2 text-white py-2 px-4 rounded-full mr-4 hover:bg-[#E8772E]-dark"
+              >
                 <MdOutlineFastfood className="" /> <span>Checkout</span>
-              </button>
+              </motion.button>
             )}
           </div>
-        </div>
+        </motion.div>
       )}{" "}
+      {/* checkout section  */}
+      <div
+        ref={menuRef}
+        onClick={() => setIsMenuOpen(false)}
+        className={`scrollbar-none scrollbar-thumb-gray-400/20 h-screen scrollbar-track-gray-100 transform duration-500 ease-in-out fixed top-0   w-full z-50 overflow-y-auto bg-[#E8772E] rounded-l-md shadow-sm ${
+          isMenuOpen ? "translate-y-0" : "translate-y-full"
+        }`}
+      >
+        Checkout content here
+      </div>
     </div>
   );
 };
