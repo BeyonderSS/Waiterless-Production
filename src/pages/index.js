96413af0285721b9b0790751b861673cfd7a14c5
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-07 21:22:01 +0530

    Feature's

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import FamousFood from "@/components/FamousFood";
 import PopularMenu from "@/components/PopularMenu";
 import Banner from "@/components/Banner";
 import AboutUs from "@/components/AboutUs";
+import Feature from "@/components/Feature";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Hero/>
       <FamousFood/>
       <PopularMenu/>
+      <Feature/>
       <AboutUs/>
       <Banner/>
       <Testimonial />
