0067c6b5bcc170484bd894dba44170dac977c253
spideyo0 <dshantanu2003@gmail.com>
2023-05-13 21:17:11 +0530

    style changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,35 +5,32 @@ const CCHero = () => {
     <>
       {/* component */}
       <div className="relative w-full min-h-screen">
-        <div className="relative bg-yellow-50 min-h-screen ">
+        
+        <div className="relative bg-[#bde6be] min-h-screen ">
           <div className="container m-auto px-6 pt-32 md:px-12 lg:pt-[4.8rem] lg:px-7">
             <div className="flex items-center flex-wrap px-2 md:px-0">
               <div className="relative lg:w-6/12 lg:py-24 xl:py-32">
-                <h1 className="font-bold text-4xl text-yellow-900 md:text-5xl lg:w-10/12">
-                  Your favorite dishes, right at your door
+                  {/* <img src="/circle.svg" alt="hello" height={100} width={100}/> */}
+                <h1 className="font-bold text-4xl text-black md:text-5xl lg:w-10/12">
+                  Your favorite dishes, right at your table
                 </h1>
                 <form action="" className="w-full mt-12">
-                  <div className="relative flex p-1 rounded-full bg-white border border-yellow-200 shadow-md md:p-2">
-                    <select
-                      className="hidden p-3 rounded-full bg-transparent md:block md:p-4"
-                      name="domain"
-                      id="domain"
-                    >
-                      <option value="design">FastFood</option>
-                      <option value="development">Restaurant</option>
-                      <option value="marketing">Marketing</option>
-                    </select>
+                  <div className="relative flex p-1 rounded-full bg-[#81c690] item-center  shadow-lg md:p-4">
+                 
+                    <div className="w-full shadow-2xl rounded-full mx-3">
                     <input
                       placeholder="Your favorite food"
-                      className="w-full p-4 rounded-full"
+                      className="w-full p-4 placeholder:text-black placeholder:bold placeholder:text-xl border-2 outline-none shadow-2xl border-black  bg-transparent rounded-full"
                       type="text"
                     />
+                    </div>
+                    
                     <button
                       type="button"
                       title="Start buying"
-                      className="ml-auto py-3 px-6 rounded-full text-center transition bg-gradient-to-b from-yellow-200 to-yellow-300 hover:to-red-300 active:from-yellow-400 focus:from-red-400 md:px-12"
+                      className="ml-auto py-3 px-6 rounded-full  text-center transition bg-gradient-to-b from-[#017f01] to-[#017f01] hover:to-[#017f01] active:from-[#017f01] focus:from-green-600 shadow-2xl md:px-12"
                     >
-                      <span className="hidden text-yellow-900 font-semibold md:block">
+                      <span className="hidden text-white font-semibold md:block">
                         Search
                       </span>
                       <svg
@@ -48,16 +45,13 @@ const CCHero = () => {
                   </div>
                 </form>
                 <p className="mt-8 text-gray-700 lg:w-10/12">
-                  Sit amet consectetur adipisicing elit.{" "}
-                  <a href="#" className="text-yellow-700">
-                    connection
-                  </a>{" "}
-                  tenetur nihil quaerat suscipit, sunt dignissimos.
+                Experience dining like never before with waiterless.tech. Order and pay with ease, eliminate wait times, and enjoy contactless service. The future of dining is here.
+                
                 </p>
               </div>
               <div className="ml-auto -mb-24 lg:-mb-56 lg:w-6/12">
                 <img
-                  src="https://tailus.io/sources/blocks/food-delivery/preview/images/food.webp"
+                  src="/food.webp"
                   className="relative"
                   alt="food illustration"
                   loading="lazy"
