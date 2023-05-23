9d98129bbf9e65b40bacc484081e3bc976ee6e3e
BeyonderSS <ssbeyonder@gmail.com>
2023-05-23 21:39:08 +0530

    loding state

--- src/pages/Dashboard/OrdersDashboard.js
+++ src/pages/Dashboard/OrdersDashboard.js
@@ -2,12 +2,29 @@ import AdminOrders from "@/components/AdminOrders";
 import NotAuth from "@/components/NotAuth";
 import { useAuth } from "@/context/AuthContext";
 import useOrders from "@/utils/useOrders";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { PropagateLoader } from "react-spinners";
 
 const OrdersDashboard = () => {
   const orders = useOrders();
-  const { user, restaurantId,role ,signInWithGoogle} = useAuth();
+  const { user, restaurantId, role, signInWithGoogle } = useAuth();
+  const [loading, setLoading] = useState(true); // State variable for loading status
+  useEffect(() => {
+    // Simulate loading for 3 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
 
+    return () => clearTimeout(timer); // Clear the timer when component unmounts
+  }, []);
+  if (loading) {
+    // Show loader while loading is true
+    return (
+      <div className="flex justify-center items-center min-h-screen pt-24 pl-80 bg-white pb-8">
+        <PropagateLoader color="#4ADE80" loading={loading} />
+      </div>
+    );
+  }
   return (
     <div>
       {role == "Admin" && <AdminOrders orders={orders} />}
